70bc93633eb95754796cc5741750f473aa9e2896
Osmair Coelho <osmair.coelho@gmail.com>
2018-05-08 14:21:45 +0100

    - Login with registration     In this module we will change the login format, allowing users to log in through their registration, because by default, Laravel only allows to log in with email.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace SON\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
+
 use SON\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -36,4 +38,27 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+	protected function credentials(Request $request)
+	{
+		$data = $request->only($this->username(), 'password');
+		$usernameKey = $this->usernameKey();
+		$data[$usernameKey] = $data[$this->username()];
+		unset($data[$this->username()]);
+		return $data;
+	}
+
+	protected function usernameKey()
+	{
+		$email = \Request::get($this->username());
+		$validator = \Validator::make([
+			'email' => $email
+		], ['email' => 'email']);
+		return $validator->fails() ? 'enrolment' : 'email';
+	}
+
+	public function username()
+	{
+		return 'username';
+	}
 }
